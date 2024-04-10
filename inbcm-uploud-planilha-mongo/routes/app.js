9ecf294357e7b396d684b81ec393f28d0372468b
Rickson-Rocha <ricksonroccha21@gmail.com>
2024-04-10 19:05:45 -0300

    [feature] rotas htpp para recursos de persistencia de dados

---
+++ inbcm-uploud-planilha-mongo/routes/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require('multer');
+const app = express();
+
+const BibliograficoController = require('../controllers/BibliograficoController.js');
+const MuseologicoController  = require('../controllers/MuseologicoController.js')
+const ArquivisticoController = require('../controllers/ArquivisticoController.js')
+
+const upload = multer({ dest: 'uploads/' });
+
+const bibliograficoController = new BibliograficoController();
+const museologicoController = new MuseologicoController();
+const arquivisticoController = new ArquivisticoController();
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/bibliografico/upload', upload.single('file'), bibliograficoController.uploudBibliograficoModel);
+app.post('/api/museologico/upload', upload.single('file'), museologicoController.uploudMuselogicoModel);
+app.post('/api/arquivistico/upload', upload.single('file'), arquivisticoController.uploudArquivisticoModel);
+
+
+
+// Conexão com o banco de dados
+const conn = require("../db/conn.js");
+conn();
+
+app.listen(3000, function() {
+    console.log("Servidor funcionando!");
+});
+
+//kkiag6cSXcij3IXY
+//ricksonroccha
