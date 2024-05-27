2a7a9d24d9294d040b01c75031b7b049dfe269e6
Rickson-Rocha <ricksonroccha21@gmail.com>
2024-05-27 20:53:40 -0300

    [feature]: adiciona campos para preencher recibo

--- service/ReciboService.ts
+++ service/ReciboService.ts
@@ -1,26 +1,15 @@
 import mongoose from "mongoose";
 import path from "path";
 import ejs from "ejs";
-import puppeteer from "puppeteer";
+import htmlToPdf from "html-pdf";
+
 import Declaracoes from "../models/Declaracao";
 import Museu from "../models/Museu";
+import Usuario from "../models/Usuario";
 
-
-interface DadosBensDeclarados {
-  totalBensDeclarados: number;
-  bensMuseologicos: number;
-  bensBibliograficos: number;
-  bensArquivisticos: number;
-}
-
-async function emitirReciboDeclaracao(
-  declaracaoId: mongoose.Types.ObjectId,
-  anoCalendario: number,
-  dataCallback: (chunk: any) => void,
-  endCallback: () => void
-): Promise<void> {
+async function gerarPDFRecibo(declaracaoId: mongoose.Types.ObjectId): Promise<Buffer> {
   try {
-    const declaracao = await Declaracoes.findById(declaracaoId).populate("museu");
+    const declaracao = await Declaracoes.findById(declaracaoId);
     if (!declaracao) {
       throw new Error(`Declaração não encontrada para o ID especificado: ${declaracaoId}`);
     }
@@ -30,12 +19,20 @@ async function emitirReciboDeclaracao(
       throw new Error(`Museu não encontrado para o ID especificado: ${declaracao.museu_id}`);
     }
 
+    const usuario = await Usuario.findById(museu.usuario);
+    if (!usuario) {
+      throw new Error(`Usuário não encontrado para o ID especificado: ${museu.usuario}`);
+    }
+    const totalBensDeclarados = 
+      (declaracao.arquivistico.quantidadeItens || 0) +
+      (declaracao.bibliografico.quantidadeItens || 0) +
+      (declaracao.museologico.quantidadeItens || 0);
+
+    // Compile o template EJS com os dados
     const templatePath = path.join(__dirname, "../templates/ejs/recibo.ejs");
-    console.log(` caminho do arquivo ${templatePath}`)
-    const dataEnvio = new Date().toLocaleString("pt-BR");
-    const dadosRecibo = {
-      anoCalendario: anoCalendario,
-      codigoIdentificador: museu._id.toString(),
+    const htmlContent = await ejs.renderFile(templatePath, {
+      anoCalendario: declaracao.anoDeclaracao,
+      codigoIdentificador: museu.codIbram,
       nomeMuseu: museu.nome,
       logradouro: museu.endereco.logradouro,
       numero: museu.endereco.numero,
@@ -43,31 +40,44 @@ async function emitirReciboDeclaracao(
       bairro: museu.endereco.bairro,
       cep: museu.endereco.cep,
       municipio: museu.endereco.municipio,
-      uf: museu.endereco.uf,    
-      // totalBensDeclarados: dadosBensDeclarados.totalBensDeclarados,
-      // bensMuseologicos: dadosBensDeclarados.bensMuseologicos,
-      // bensBibliograficos: dadosBensDeclarados.bensBibliograficos,
-      // bensArquivisticos: dadosBensDeclarados.bensArquivisticos,
-      nomeDeclarante: declaracao.responsavelEnvio,
-      horaData: dataEnvio,
+      uf: museu.endereco.uf,
+      nomeDeclarante: usuario.nome,
+      horaData: new Date().toLocaleString("pt-BR"),
       numeroRecibo: declaracao.hashDeclaracao,
+      totalBensDeclarados: totalBensDeclarados.toString(),
+      bensMuseologicos: declaracao.museologico.quantidadeItens.toString(),
+      bensBibliograficos: declaracao.bibliografico.quantidadeItens.toString(),
+      bensArquivisticos:  declaracao.arquivistico.quantidadeItens.toString(),
+    });
+
+    // Configuração para a conversão de HTML para PDF
+    const pdfOptions = {
+      format: "A4", // ou qualquer outro formato suportado que desejar
+      border: {
+        top: "1cm",
+        right: "1cm",
+        bottom: "1cm",
+        left: "1cm"
+      },
+      timeout: 30000,
     };
 
-    const html = await ejs.renderFile(templatePath, dadosRecibo);
+    // Converte o HTML para PDF
+    return new Promise((resolve, reject) => {
+      htmlToPdf.create(htmlContent, pdfOptions).toBuffer((err, buffer) => {
+        if (err) {
+          console.error("Erro ao gerar o PDF:", err);
+          reject(err);
+        } else {
+          resolve(buffer);
+        }
+      });
+    });
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setContent(html, { waitUntil: "networkidle0" });
-    const pdfBuffer = await page.pdf({ format: "A4" });
-    await browser.close();
-
-    dataCallback(pdfBuffer);
-    endCallback();
-   
   } catch (error) {
-    console.error("Erro ao emitir recibo de declaração:", error);
-    throw error;
+    console.error("Erro ao gerar o recibo:", error);
+    throw new Error("Erro ao gerar o recibo.");
   }
 }
 
-export { emitirReciboDeclaracao };
+export { gerarPDFRecibo };
