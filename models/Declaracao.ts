a9c57e3719f85ca31ce06274170eb752ec0f32a4
Rickson-Rocha <ricksonroccha21@gmail.com>
2024-06-19 19:55:52 -0300

    [feature]: atualiza model de Declaracao com enums de tipoEnvio e Status

--- models/Declaracao.ts
+++ models/Declaracao.ts
@@ -1,85 +1,103 @@
 import mongoose, { Schema, Document } from "mongoose";
+import { Status } from "../enums/Status";
+import { TipoEnvio } from "../enums/tipoEnvio";
+import {gerarData} from "../utils/dataUtils"
 
-interface Arquivo {
-  nome?: string;
-  caminho?: string;
-  status: string;
+export interface HistoricoVersao {
+  nome: string;
+  caminho: string;
+  dataEnvio: string;
+  tipoEnvio: TipoEnvio;
   pendencias: string[];
   quantidadeItens: number;
-  hashArquivo?: string;
-  tipoEnvio?: 'enviado' | 'reenviado';
-  dataEnvio: Date;
   versao: number;
-  historicoVersoes?: {
-    nome: string;
-    caminho: string;
-    dataEnvio: Date;
-    tipoEnvio: 'enviado' | 'reenviado';
-  }[];
-};
+}
+
+const HistoricoVersaoSchema = new Schema<HistoricoVersao>({
+  nome: String,
+  caminho: String,
+  dataEnvio: { type: String, default: gerarData },
+  tipoEnvio: {
+    type: String,
+    enum: Object.values(TipoEnvio),
+  },
+  pendencias: [String],
+  quantidadeItens: { type: Number, default: 0 },
+  versao: { type: Number, default: 0 },
+}, { _id: false });
+
+export interface Arquivo {
+  nome?: string;
+  caminho?: string;
+  status: Status;
+  pendencias?: string[];
+  quantidadeItens: number;
+  hashArquivo?: string;
+  tipoEnvio?: TipoEnvio;
+  dataEnvio: string;
+  versao: number;
+  historicoVersoes: HistoricoVersao[];
+}
 
 const ArquivoSchema = new Schema<Arquivo>({
   nome: String,
   caminho: String,
   status: {
     type: String,
-    enum: ["em processamento", "em análise", "com pendências", "não enviado"],
-    default: "não enviado",
+    enum: Object.values(Status),
+    default: Status.NaoEnviado,
   },
   pendencias: [String],
   quantidadeItens: { type: Number, default: 0 },
   hashArquivo: String,
+  dataEnvio: { type: String, default: gerarData },
+  versao: { type: Number, default: 0 },
+  historicoVersoes: [HistoricoVersaoSchema]
 }, { _id: false });
 
-  interface DeclaracaoModel extends Document {
-    museu_id: mongoose.Types.ObjectId;
-    museu_nome: string;
-    anoDeclaracao: string;
-    responsavelEnvio: mongoose.Types.ObjectId;
-    hashDeclaracao: string;
-    dataCriacao: Date;
-    dataAtualizacao?: Date;
-    totalItensDeclarados?: number;
-    status: string;
-    arquivistico: Arquivo;
-    bibliografico: Arquivo;
-    museologico: Arquivo;
-    retificacao: boolean;
-    retificacaoRef: mongoose.Types.ObjectId;
-    pendente: boolean;
-    versao: number;
-  }
+export interface DeclaracaoModel extends Document {
+  museu_id: mongoose.Types.ObjectId;
+  museu_nome: string;
+  anoDeclaracao: string;
+  responsavelEnvio: mongoose.Types.ObjectId;
+  hashDeclaracao: string;
+  dataCriacao: String;
+  dataAtualizacao?: String;
+  totalItensDeclarados?: number;
+  status: Status;
+  arquivistico: Arquivo;
+  bibliografico: Arquivo;
+  museologico: Arquivo;
+  retificacao: boolean;
+  retificacaoRef: mongoose.Types.ObjectId;
+  pendente: boolean;
+  versao: number;
+}
 
-  const DeclaracaoSchema = new Schema<DeclaracaoModel>({
-    museu_id: { type: Schema.Types.ObjectId, ref: 'Museu', required: true },
-    museu_nome: String,
-    versao: { type: Number, default: 0 },
-    anoDeclaracao: String,
-    responsavelEnvio: { type: Schema.Types.ObjectId, ref: 'usuarios', required: true },
-    hashDeclaracao: String,
-    dataCriacao: { type: Date, default: Date.now() },
-    dataAtualizacao: { type: Date },
-    retificacao: { type: Boolean, default: false },
-    retificacaoRef: { type: Schema.Types.ObjectId, ref: 'Declaracoes' },
-    totalItensDeclarados: { type: Number },
-    pendente: { type: Boolean, default: false },
-    status: {
-      type: String,
-      enum: [
-        "em processamento",
-        "processada",
-        "com pendências",
-        "em análise por técnicos do IBRAM",
-        "em análise",
-        "finalizada",
-      ],
-      default: "em análise",
-    },
-    arquivistico: ArquivoSchema,
-    bibliografico: ArquivoSchema,
-    museologico: ArquivoSchema,
-  });
+export type ArquivoTypes = 'arquivisticoArquivo' | 'bibliograficoArquivo' | 'museologicoArquivo';
+
+const DeclaracaoSchema = new Schema<DeclaracaoModel>({
+  museu_id: { type: Schema.Types.ObjectId, ref: 'Museu', required: true },
+  museu_nome: String,
+  versao: { type: Number, default: 0 },
+  anoDeclaracao: String,
+  responsavelEnvio: { type: Schema.Types.ObjectId, ref: 'usuarios', required: true },
+  hashDeclaracao: String,
+  dataCriacao: { type: Date, default: gerarData },
+  dataAtualizacao: { type: String, default: gerarData },
+  retificacao: { type: Boolean, default: false },
+  retificacaoRef: { type: Schema.Types.ObjectId, ref: 'Declaracoes' },
+  totalItensDeclarados: { type: Number },
+  pendente: { type: Boolean, default: false },
+  status: {
+    type: String,
+    enum: Object.values(Status),
+    default: Status.EmAnalise,
+  },
+  arquivistico: ArquivoSchema,
+  bibliografico: ArquivoSchema,
+  museologico: ArquivoSchema,
+});
 
 export const Declaracoes = mongoose.model<DeclaracaoModel>("Declaracoes", DeclaracaoSchema);
-export { DeclaracaoModel };
 export default Declaracoes;
