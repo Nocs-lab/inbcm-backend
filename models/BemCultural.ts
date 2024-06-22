7e1bf73247958fa8b21025b0ced34e650bfa648b
Rickson-Rocha <ricksonroccha21@gmail.com>
2024-06-21 23:01:20 -0300

    [feature]:adiciona alias aos models arquivistico,bibliografico e museologico para solucionar problema de persistencia de dados ao salvar ou retificar uma declaracao

--- models/BemCultural.ts
+++ models/BemCultural.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema } from "mongoose";
 // Defina o modelo base genérico
 const BemCulturalSchema = new mongoose.Schema({
   titulo: { type: String},
-  condicoesReproducao: { type: String,alias:"condicoesDeReproducao"},
+  condicoesReproducao: { type: String,alias:"condicoesreproducao"},
   midiasRelacionadas: { type: [String] }, // Pode ser uma lista de URLs ou referências
   declaracao_ref: { type: Schema.Types.ObjectId, required: true, ref: "Declaracoes" },
 });
