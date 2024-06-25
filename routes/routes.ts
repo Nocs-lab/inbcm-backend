96f03b77c1875bc251010b2acb9104919dafcd59
Bruno Pithon <brunopithondev@gmail.com>
2024-06-24 21:24:12 -0300

    feat: rotas para crud de profile e user

--- routes/routes.ts
+++ routes/routes.ts
@@ -3,14 +3,84 @@ import uploadMiddleware from "../middlewares/UploadMiddleware";
 import DeclaracaoController from "../controllers/DeclaracaoController";
 import MuseuController from "../controllers/MuseuController";
 import ReciboController from "../controllers/ReciboController";
+import ProfileController from "../controllers/ProfileController";
 import AuthService from "../service/AuthService";
-import { adminMiddleware, userMiddleware } from "../middlewares/AuthMiddlewares";
+import { adminMiddleware, userMiddleware, permissionCheckMiddleware } from "../middlewares/AuthMiddlewares";
+import UsuarioController from "../controllers/UsuarioController";
 
 
 const routes = express.Router();
 const reciboController = new ReciboController();
 const declaracaoController = new DeclaracaoController();
 const authService = new AuthService()
+
+
+
+//Profile
+
+routes.post('/profile', ProfileController.createProfile);
+routes.get('/profiles',permissionCheckMiddleware('getProfiles'), ProfileController.getProfiles);
+routes.get('/profile/:id',permissionCheckMiddleware('getProfileById'), ProfileController.getProfileById);
+routes.put('/profile/:id', ProfileController.updateProfile);
+routes.delete('/profile/:id', ProfileController.deleteProfile);
+
+
+//Usuário
+routes.post('/user', UsuarioController.registerUsuario);
+routes.get('/users', UsuarioController.getUsuarios);
+routes.get('/user/:id', UsuarioController.getUsuarioPorId);
+routes.put('/user/:id', UsuarioController.atualizarUsuario);
+
+
+
+
+
+//Auth
+routes.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body
+  const { admin } = req.query
+  const { token, refreshToken, user } = await authService.login({ email, password, admin: admin === "true" })
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 60 * 1000),
+    maxAge: 60 * 60 * 1000,
+    sameSite: "strict",
+    secure: true,
+    signed: true
+  })
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    sameSite: "strict",
+    secure: true,
+    signed: true
+  })
+  res.json({
+    name: user.nome,
+    email: user.email
+  })
+})
+
+routes.post("/auth/refresh", async (req, res) => {
+  const { refreshToken } = req.signedCookies
+  try {
+    const { token } = await authService.refreshToken({ refreshToken })
+    res.cookie("token", token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 60 * 60 * 1000),
+      maxAge: 60 * 60 * 1000,
+      sameSite: "strict",
+      secure: true,
+      signed: true
+    })
+
+    res.status(200).send()
+  } catch (error) {
+    res.status(401).send()
+  }
+})
 
 //Museu
 routes.post('/criarMuseu', adminMiddleware, MuseuController.criarMuseu);
@@ -44,51 +114,6 @@ routes.get("/dashboard/status", declaracaoController.getDeclaracoesPorStatus);
 //Recibo
 routes.get("/recibo/:idDeclaracao",userMiddleware,reciboController.gerarRecibo);
 
-routes.post("/auth/login", async (req, res) => {
-  const { email, password } = req.body
-  const { admin } = req.query
-  const { token, refreshToken, user } = await authService.login({ email, password, admin: admin === "true" })
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 60 * 60 * 1000),
-    maxAge: 60 * 60 * 1000,
-    sameSite: "strict",
-    secure: true,
-    signed: true
-  })
-  res.cookie("refreshToken", refreshToken, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    sameSite: "strict",
-    secure: true,
-    signed: true
-  })
-
-  res.json({
-    name: user.nome,
-    email: user.email
-  })
-})
-
-routes.post("/auth/refresh", async (req, res) => {
-  const { refreshToken } = req.signedCookies
-  try {
-    const { token } = await authService.refreshToken({ refreshToken })
-    res.cookie("token", token, {
-      httpOnly: true,
-      expires: new Date(Date.now() + 60 * 60 * 1000),
-      maxAge: 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: true,
-      signed: true
-    })
-
-    res.status(200).send()
-  } catch (error) {
-    res.status(401).send()
-  }
-})
 
 export default routes;
