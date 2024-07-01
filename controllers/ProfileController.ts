2db3d8e31859fb111f5c703fc4f06e8387308030
Bruno Pithon <brunopithondev@gmail.com>
2024-07-01 19:22:17 -0300

    feat: implementação do método para adicionar permissões em profile

--- controllers/ProfileController.ts
+++ controllers/ProfileController.ts
@@ -1,12 +1,51 @@
 import { Request, Response } from 'express';
 import { Profile } from '../models/Profile';
+import { Permission } from '../models/Permission';
+import mongoose, { Types } from 'mongoose';
 
 class ProfileController {
 
+  public async addPermissions(req: Request, res: Response): Promise<void> {
+    try {
+      const { profileId, permissionIds } = req.body;
+
+      if (!profileId || !permissionIds || !Array.isArray(permissionIds)) {
+        res.status(400).json({ message: 'Profile ID and an array of Permission IDs are required.' });
+        return;
+      }
+
+      const profile = await Profile.findById(profileId);
+      if (!profile) {
+        res.status(404).json({ message: 'Profile not found.' });
+        return;
+      }
+
+      const validPermissionIds: mongoose.Types.ObjectId[] = [];
+      for (const id of permissionIds) {
+        const permission = await Permission.findById(id);
+        if (permission) {
+          validPermissionIds.push(permission._id as mongoose.Types.ObjectId);
+        } else {
+          res.status(404).json({ message: `Permission not found for ID: ${id}` });
+          return;
+        }
+      }
+
+      profile.permissions = validPermissionIds; // Reseta as permissões existentes e adiciona as novas permissões
+      await profile.save();
+
+      res.status(200).json({ message: 'Permissions added successfully.', profile });
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred.', error });
+    }
+  }
+
+
+
   public async createProfile(req: Request, res: Response): Promise<Response> {
     try {
-      const { name, permissions } = req.body;
-      const profile = new Profile({ name, permissions });
+      const { name, description } = req.body;
+      const profile = new Profile({ name, description });
       await profile.save();
       return res.status(201).json(profile);
     } catch (error) {
@@ -42,8 +81,8 @@ class ProfileController {
   public async updateProfile(req: Request, res: Response): Promise<Response> {
     try {
       const { id } = req.params;
-      const { name, permissions } = req.body;
-      const profile = await Profile.findByIdAndUpdate(id, { name, permissions }, { new: true });
+      const { name, description } = req.body;
+      const profile = await Profile.findByIdAndUpdate(id, { name, description }, { new: true });
       if (!profile) {
         return res.status(404).json({ message: 'Perfil não encontrado' });
       }
