0f87059f55404b23bc633870ae783255b671b6ed
Rickson-Rocha <ricksonroccha21@gmail.com>
2024-07-03 06:12:50 -0300

    [feature]:adiciona alias  na propriedade midiasRelacionadas

--- models/BemCultural.ts
+++ models/BemCultural.ts
@@ -4,8 +4,8 @@ import mongoose, { Schema } from "mongoose";
 const BemCulturalSchema = new mongoose.Schema({
   titulo: { type: String},
   condicoesReproducao: { type: String,alias:"condicoesreproducao"},
-  midiasRelacionadas: { type: [String] }, // Pode ser uma lista de URLs ou referências
-  declaracao_ref: { type: Schema.Types.ObjectId, required: true, ref: "Declaracoes" },
+  midiasRelacionadas: { type: [String], alias:"midiasrelacionadas"},
+  declaracao_ref: [{ type: Schema.Types.ObjectId, required: true, ref: "Declaracoes" }],
   versao: { type: Number, default: 0 } // Adicionando campo de versão
 });
 
