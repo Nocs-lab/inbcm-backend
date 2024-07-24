5c4025f906f76c738ca8e7e41da7966f6b49f5f8
Bruno Pithon <brunopithondev@gmail.com>
2024-07-24 10:52:51 -0300

    refactor: reescrita da validacao e retorno do erro

--- src/middlewares/UploadMiddleware.ts
+++ src/middlewares/UploadMiddleware.ts
@@ -17,20 +17,20 @@ const upload = multer({ limits: { fieldSize: 1024 * 1024 * 1024 } }).fields([
 
 const uploadMiddleware: RequestHandler = (req, res, next) => {
   upload(req, res, (err: any) => {
-    if (err instanceof MulterError) {
-      return res.status(400).json({ message: 'Erro ao fazer submissão do(s) arquivo(s): ' + err.message });
-    } else if (err instanceof Error) {
-      return res.status(500).json({ message: 'Erro não mapeado ao fazer submissão do(s) arquivo(s): ' + err.message });
+    if (err) {
+      const errorMessage =
+        err instanceof MulterError
+          ? 'Erro ao fazer submissão do(s) arquivo(s): ' + err.message
+          : 'Erro não mapeado ao fazer submissão do(s) arquivo(s): ' + err.message;
+      return res.status(err instanceof MulterError ? 400 : 500).json({ message: errorMessage });
     }
     const uploadReq = req as SubmissionRequest;
 
-    if (
-      !uploadReq.files.arquivistico &&
-      !uploadReq.files.bibliografico &&
-      !uploadReq.files.museologico
-    ) {
-      return res.status(400).json({ message: 'Pelo menos um arquivo deve ser enviado.' });
-    }
+     const { arquivistico, bibliografico, museologico } = uploadReq.files;
+
+  if (!arquivistico && !bibliografico && !museologico) {
+    return res.status(400).json({ message: 'Pelo menos um arquivo deve ser enviado.' });
+  }
 
     next();
   });
