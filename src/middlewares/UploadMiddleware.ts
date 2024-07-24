c35bade885b92ff0b21b673ebe804a6aac941a48
Bruno Pithon <brunopithondev@gmail.com>
2024-07-24 09:04:35 -0300

    fix: corrige tipagem em UploadMiddleware

--- src/middlewares/UploadMiddleware.ts
+++ src/middlewares/UploadMiddleware.ts
@@ -1,26 +1,39 @@
-import multer from "multer"
-import { RequestHandler } from "express"
+import multer, { MulterError } from 'multer';
+import { Request, RequestHandler } from 'express';
 
-// Crie o middleware de upload para lidar com vários arquivos
-const uploadMiddleware: RequestHandler = (req, res, next) => {
-  multer({ limits: { fieldSize: 1024 * 1024 * 1024 } }).fields([
-    { name: "arquivistico", maxCount: 1 },
-    { name: "bibliografico", maxCount: 1 },
-    { name: "museologico", maxCount: 1 }
-  ])(req, res, (err) => {
-    if (err) {
-      console.log(err)
-      return res
-        .status(400)
-        .json({ message: "Erro ao fazer upload dos arquivos:" + err })
-    }
-    // Verificar se os arquivos foram enviados corretamente
-    if (!req.files) {
-      return res.status(400).json({ message: "Nenhum arquivo enviado." })
-    }
-
-    next()
-  })
+interface SubmissionRequest extends Request {
+  files: {
+    arquivistico?: Express.Multer.File[];
+    bibliografico?: Express.Multer.File[];
+    museologico?: Express.Multer.File[];
+  };
 }
 
-export default uploadMiddleware
+const upload = multer({ limits: { fieldSize: 1024 * 1024 * 1024 } }).fields([
+  { name: 'arquivistico', maxCount: 1 },
+  { name: 'bibliografico', maxCount: 1 },
+  { name: 'museologico', maxCount: 1 },
+]);
+
+const uploadMiddleware: RequestHandler = (req, res, next) => {
+  upload(req, res, (err: any) => {
+    if (err instanceof MulterError) {
+      return res.status(400).json({ message: 'Erro ao fazer submissão do(s) arquivo(s): ' + err.message });
+    } else if (err instanceof Error) {
+      return res.status(500).json({ message: 'Erro não mapeado ao fazer submissão do(s) arquivo(s): ' + err.message });
+    }
+    const uploadReq = req as SubmissionRequest;
+
+    if (
+      !uploadReq.files.arquivistico &&
+      !uploadReq.files.bibliografico &&
+      !uploadReq.files.museologico
+    ) {
+      return res.status(400).json({ message: 'Pelo menos um arquivo deve ser enviado.' });
+    }
+
+    next();
+  });
+};
+
+export default uploadMiddleware;
