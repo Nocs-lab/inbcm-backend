1f961081f25cca9d2f4b54499b7f202e22416f8e
Vitor Daniel <vitor036daniel@gmail.com>
2024-11-26 01:25:56 -0300

    fix: remover referência as credenciais do minio

--- src/config.ts
+++ src/config.ts
@@ -17,8 +17,8 @@ const parsed = {
   MINIO_ENDPOINT: process.env.MINIO_ENDPOINT ?? "localhost",
   MINIO_PORT: process.env.MINIO_PORT ?? "9000",
   MINIO_USE_SSL: process.env.MINIO_USE_SSL ?? "false",
-  MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY ?? "minioadmin",
-  MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY ?? "minioadmin"
+  MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY ?? "",
+  MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY ?? ""
 }
 logger.info("Carregando configurações...")
 
