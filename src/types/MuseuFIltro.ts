b3ef32cb4aaf949602aa518b59fc94c8a58368c6
Rickson Rocha <ricksonroccha21@gmail.com>
2025-01-09 17:43:46 -0300

    [feat]:adiciona propriedade nome a interface MuseuFiltro

--- src/types/MuseuFIltro.ts
+++ src/types/MuseuFIltro.ts
@@ -1,4 +1,5 @@
 export interface MuseuFiltro {
   usuario?: null // Filtro de museus sem vínculo com usuários
-  $text?: { $search: string } // Filtro de busca usando índice de texto
+  $text?: { $search: string }
+  nome?: { $regex: string; $options: string } // Filtro de busca usando índice de texto
 }
