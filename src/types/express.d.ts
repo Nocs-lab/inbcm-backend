fa7b73c9443df346ee5e8642ca64a789d848bdf4
Rickson Rocha <ricksonroccha21@gmail.com>
2025-01-10 08:27:54 -0300

    [feat]:adiciona nova interface IRequestUser

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,13 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Request } from "express"
+import { IUsuario } from "../models"
 
 declare global {
   namespace Express {
     interface Request {
-      user: {
-        id: string
-        admin: boolean
-      }
+      user: IUsuario
+    }
+  }
+}
+
+interface IRequestUser {
+  id: string
+  admin: boolean
+  profile: IProfile | Types.ObjectId | string
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IRequestUser
     }
   }
 }
