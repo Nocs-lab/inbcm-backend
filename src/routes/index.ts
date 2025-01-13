871c45e5bc3c2358d81776c4f4a0a9eee3dcbd73
Vitor Daniel <vitor036daniel@gmail.com>
2025-01-13 16:10:58 -0300

    feat: retornar versao

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import express from "express"
 import publicRoutes from "./public"
 import adminRoutes from "./admin"
+import config from "../config"
 
 const routes = express.Router()
+
+routes.use((_req, res, next) => {
+  res.setHeader("x-version", config.SHORT_SHA)
+  next()
+})
 
 routes.use("/public", publicRoutes)
 routes.use("/admin", adminRoutes)
