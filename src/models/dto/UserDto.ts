1547d52023118f0e80786d2d3b1fe13d9ca613cd
Rickson Rocha <ricksonroccha21@gmail.com>
2025-01-28 23:14:18 -0300

    [feat]:cria UpdateUserDto para separar o model da regra de negocio

---
+++ src/models/dto/UserDto.ts
@@ -0,0 +1,9 @@
+export class UpdateUserDto {
+  nome?: string
+  email?: string
+  perfil?: string
+  especialidadeAnalista?: string[]
+  museus?: string[]
+  desvincularMuseus?: string[]
+  cpf?: string
+}
