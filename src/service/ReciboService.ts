36951cdf1fd3ebfba0caaed53ea65d167c1d4343
Rickson Rocha <ricksonroccha21@gmail.com>
2025-02-20 12:51:07 -0300

    [refact]:separa responsabilidades na criacao do recibo original

--- src/service/ReciboService.ts
+++ src/service/ReciboService.ts
@@ -1,79 +1,23 @@
 import mongoose from "mongoose"
-import { DeclaracaoModel, Declaracoes, Museu, Usuario } from "../models"
-import PdfPrinter from "pdfmake"
-import { IMuseu } from "../models/Museu"
-import { IUsuario } from "../models/Usuario"
 import path from "path"
 import { DataUtils } from "../utils/dataUtils"
 import { TDocumentDefinitions } from "pdfmake/interfaces"
 import HTTPError from "../utils/error"
-
-const camposAmigaveis = {
-  museologico: {
-    nderegistro: "Número de Registro",
-    outrosnumeros: "Outros Números",
-    situacao: "Situação",
-    denominacao: "Denominação",
-    titulo: "Título",
-    autor: "Autor",
-    classificacao: "Classificação",
-    resumodescritivo: "Resumo Descritivo",
-    dimensoes: "Dimensões",
-    materialtecnica: "Material/Técnica",
-    estadodeconservacao: "Estado de Conservação",
-    localdeproducao: "Local de Produção",
-    datadeproducao: "Data de Produção",
-    condicoesdereproducao: "Condições de Reprodução",
-    midiasrelacionadas: "Mídias Relacionadas"
-  },
-  bibliografico: {
-    nderegistro: "Número de Registro",
-    outrosnumeros: "Outros Números",
-    situacao: "Situação",
-    titulo: "Título",
-    tipo: "Tipo",
-    identificacaoderesponsabilidade: "Identificação de Responsabilidade",
-    localdeproducao: "Local de Produção",
-    editora: "Editora",
-    datadeproducao: "Data de Produção",
-    dimensaofisica: "Dimensão Física",
-    materialtecnica: "Material/Técnica",
-    encadernacao: "Encadernação",
-    resumodescritivo: "Resumo Descritivo",
-    estadodeconservacao: "Estado de Conservação",
-    assuntoprincipal: "Assunto Principal",
-    assuntocronologico: "Assunto Cronológico",
-    assuntogeografico: "Assunto Geográfico",
-    condicoesdereproducao: "Condições de Reprodução",
-    midiasrelacionadas: "Mídias Relacionadas"
-  },
-  arquivistico: {
-    coddereferencia: "Código de Referência",
-    titulo: "Título",
-    data: "Data",
-    niveldedescricao: "Nível de Descrição",
-    dimensaoesuporte: "Dimensão e Suporte",
-    nomedoprodutor: "Nome do Produtor",
-    historiaadministrativabiografia: "História Administrativa/Biografia",
-    historiaarquivistica: "História Arquivística",
-    procedencia: "Procedência",
-    ambitoeconteudo: "Âmbito e Conteúdo",
-    sistemadearranjo: "Sistema de Arranjo",
-    condicoesdereproducao: "Condições de Reprodução",
-    existenciaelocalizacaodosoriginais:
-      "Existência e Localização dos Originais",
-    notassobreconservacao: "Notas sobre Conservação",
-    pontosdeacessoeindexacaodeassuntos:
-      "Pontos de Acesso e Indexação de Assuntos",
-    midiasrelacionadas: "Mídias Relacionadas"
-  }
-}
+import {
+  buscaDeclaracao,
+  buscaMuseu,
+  buscaUsuario,
+  MapeadorCamposPercentual,
+  formatarDadosRecibo
+} from "./utilsDocuments"
+import { DeclaracaoModel } from "../models"
+import PdfPrinter from "pdfmake"
 
 const gerarTabela = (
   tipo: "museologico" | "bibliografico" | "arquivistico",
-  declaracao: any
+  declaracao: DeclaracaoModel
 ) => {
-  const campos = camposAmigaveis[tipo]
+  const campos = MapeadorCamposPercentual[tipo]
   const porcentagemPorCampo = declaracao[tipo]?.porcentagemPorCampo || []
 
   return {
@@ -112,116 +56,6 @@ const gerarTabela = (
     }
   }
 }
-/**
- * Obtém uma declaração pelo seu ID.
- *
- * @param declaracaoId - O ID da declaração a ser obtida.
- * @returns Uma promessa que resolve com a declaração encontrada.
- * @throws Um erro se a declaração não for encontrada.
- */
-async function buscaDeclaracao(declaracaoId: mongoose.Types.ObjectId) {
-  const declaracao = await Declaracoes.findById(declaracaoId)
-  if (!declaracao) {
-    throw new HTTPError(
-      `Declaração não encontrada para o ID especificado: ${declaracaoId}`,
-      404
-    )
-  }
-  return declaracao
-}
-
-/**
- * Obtém um museu pelo seu ID.
- *
- * @param museuId - O ID do museu a ser obtido.
- * @returns Uma promessa que resolve com o museu encontrado.
- * @throws Um erro se o museu não for encontrado.
- */
-async function buscaMuseu(museuId: mongoose.Types.ObjectId) {
-  const museu = await Museu.findById(museuId)
-  if (!museu) {
-    throw new HTTPError(
-      `Museu não encontrado para o ID especificado: ${museuId}`,
-      404
-    )
-  }
-  return museu
-}
-
-async function buscaUsuario(usuarioId: mongoose.Types.ObjectId) {
-  const usuario = await Usuario.findById(usuarioId)
-  if (!usuario) {
-    throw new HTTPError(
-      `Usuário não encontrado para o ID especificado: ${usuarioId}`,
-      404
-    )
-  }
-  return usuario
-}
-
-/**
- * Formata os dados de uma declaração para o recibo.
- *
- * @param declaracao - A declaração a ser formatada.
- * @param museu - O museu relacionado à declaração.
- * @param usuario - O usuário relacionado ao museu.
- * @returns Os dados formatados para o recibo.
- */
-function formatarDadosRecibo(
-  declaracao: DeclaracaoModel,
-  museu: IMuseu,
-  usuario: IUsuario
-) {
-  const totalBensDeclarados =
-    (declaracao.arquivistico?.quantidadeItens || 0) +
-    (declaracao.bibliografico?.quantidadeItens || 0) +
-    (declaracao.museologico?.quantidadeItens || 0)
-
-  const formatValue = (value: number | undefined): string =>
-    value === undefined || value === 0 ? "0" : value.toString()
-
-  const tipoDeclaracao = declaracao.retificacao ? "retificadora" : "original"
-
-  const verificarPendencias = (pendencias: any[] | undefined): string => {
-    return pendencias && pendencias.length > 0 ? "Sim" : "---"
-  }
-
-  return {
-    anoCalendario: declaracao.anoDeclaracao,
-    codigoIdentificador: museu.codIbram,
-    nomeMuseu: museu.nome,
-    logradouro: museu.endereco.logradouro,
-    numero: museu.endereco.numero,
-    complemento: museu.endereco.complemento,
-    bairro: museu.endereco.bairro,
-    cep: museu.endereco.cep,
-    municipio: museu.endereco.municipio,
-    uf: museu.endereco.uf,
-    nomeDeclarante: usuario.nome,
-    data: DataUtils.gerarDataFormatada(declaracao.dataRecebimento),
-    hora: DataUtils.gerarHoraFormatada(declaracao.dataRecebimento),
-    numeroRecibo: declaracao.hashDeclaracao,
-    totalBensDeclarados: formatValue(totalBensDeclarados),
-    bensMuseologicos: formatValue(declaracao.museologico?.quantidadeItens),
-    bensBibliograficos: formatValue(declaracao.bibliografico?.quantidadeItens),
-    bensArquivisticos: formatValue(declaracao.arquivistico?.quantidadeItens),
-    tipoDeclaracao: tipoDeclaracao.toUpperCase(),
-    statusDeclaracao: declaracao.status,
-    statusArquivoArquivistico: declaracao.arquivistico?.status || "---",
-    statusArquivoMuseologico: declaracao.museologico?.status || "---",
-    statusArquivoBibliografico: declaracao.bibliografico?.status || "---",
-    pendenciaisArquivoMuseologico: verificarPendencias(
-      declaracao.museologico?.pendencias
-    ),
-    pendenciaisArquivoArquivsitico: verificarPendencias(
-      declaracao.arquivistico?.pendencias
-    ),
-    pendenciaisArquivoBibliografico: verificarPendencias(
-      declaracao.bibliografico?.pendencias
-    )
-  }
-}
-// Mapeamento para os campos de cada tipo de arquivo
 
 /**
  * Gera o PDF do recibo com base no ID da declaração.
