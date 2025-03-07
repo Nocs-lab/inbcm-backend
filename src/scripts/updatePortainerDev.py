6578b921912ac02f56b87cdf4f38d580a14903eb
Pedro_Sales <69434231+NwDPiter@users.noreply.github.com>
2025-03-07 23:32:05 +0000

    Atualização de script

--- src/scripts/updatePortainerDev.py
+++ src/scripts/updatePortainerDev.py
@@ -1,12 +1,11 @@
 from playwright.sync_api import sync_playwright
-from time import sleep
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 site_login_inbcm = os.getenv("site_login_inbcm")
-page_stacks = os.getenv("page_stacks")
+page_stacks_dev = os.getenv("page_stacks_dev")
 userInbcm = os.getenv("userInbcm")
 pwd = os.getenv("pwd")
 
@@ -18,13 +17,11 @@ with sync_playwright() as p:
     pagina.fill('xpath=//*[@id="username"]',userInbcm)
     pagina.fill('xpath=//*[@id="password"]',pwd)
     pagina.click('xpath=//*[@id="view"]/div/div/div/div[3]/div/form/div[4]/div/button') #Button login
-    sleep(2)
-    pagina.goto(page_stacks)
-    sleep(2)
-    pagina.locator('a:has-text("inbcm_dev")').click()
-    sleep(2)
+    pagina.wait_for_load_state("networkidle")
+    pagina.goto(page_stacks_dev)
+    pagina.wait_for_load_state("networkidle")
     pagina.locator('a:has-text("Editor")').click()
-    sleep(2)
+    pagina.wait_for_load_state("networkidle")
     pagina.click('xpath=//*[@id="view"]/div[1]/div/rd-widget/div/rd-widget-body/div/div/div/div/div[2]/form/div[5]/div[2]/div/button') #Click button update
-    sleep(2)
+    pagina.wait_for_load_state("networkidle")
     pagina.click('xpath=/html/body/div[2]/div/div/div[3]/button[2]') #Click update
