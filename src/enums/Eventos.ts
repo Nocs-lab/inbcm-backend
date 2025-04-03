36e8a900cc9bd9e02315f65fee578dbc48c594ef
Bruno Pithon <brunopithon@velty.com.br>
2025-04-03 17:14:00 -0300

    [fix] remove adiciona e tualiza eventos

--- src/enums/Eventos.ts
+++ src/enums/Eventos.ts
@@ -1,11 +1,11 @@
 export enum Eventos {
   EnvioDeclaracao = "Envio de declaração",
-  EnvioRetificacao = "Envio de retificação",
-  EnvioParaAnalise = "Declaração enviada para análise para  o(a) analista ",
-  EnvioParaAnalista = "Declaração enviada analista",
-  ConclusaoAnalise = "Conclusão de análise",
-  MudancaStatus = "Mudança de status de declaração",
   RetificacaoDeclaracao = "Retificação de declaração",
+  EnvioParaAnalise = "Declaração enviada para análise",
+  ConclusaoAnalise = "Conclusão de análise", //verificar se é realmente utilizado
+  MudancaStatus = "Alteração de situação",
   ExclusaoDeclaracao = "Exclusão de declaração",
-  DeclaracaoRestaurada = "Declaração restaurada para situação Recebida"
+  DeclaracaoRestaurada = `Declaração restaurada para situação "Recebida"`,
+  FinalizacaoAnalise = "Alteração de situação após análise",
+  MudancaDeAnalista = "Mudança de analista"
 }
