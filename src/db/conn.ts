f233f9ab8aae2d674e162b88877df6a29f7853a8
Vitor Daniel <vitor036daniel@gmail.com>
2025-12-22 14:17:46 -0300

    feat: Salvar logs de errors no banco de dados

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -8,6 +8,25 @@ async function main() {
 
     await mongoose.connect(config.DB_URL!)
     logger.info("Conectado ao MongoDB!")
+
+    const db = mongoose.connection.db
+
+    if (!db) {
+      throw new Error("Não foi possível obter a instância do banco de dados.")
+    }
+
+    const collections = await db.listCollections({ name: "log_erros" }).toArray()
+
+    if (collections.length === 0) {
+      await db.createCollection("log_erros", {
+        timeseries: {
+          timeField: "timestamp",
+          metaField: "meta",
+          granularity: "seconds"
+        }
+      })
+      logger.info("Coleção de logs criada com sucesso!")
+    }
   } catch (error) {
     logger.error(`Erro: ${error}`)
   }
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,8 @@
 import winston from "winston"
+import { MongoDB } from "winston-mongodb"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const logger = winston.createLogger({
   level: "http",
@@ -12,7 +16,16 @@ const logger = winston.createLogger({
       (info) => `${info.timestamp} ${info.level}: ${info.message}`
     )
   ),
-  transports: [new winston.transports.Console()],
+  transports: [
+    new winston.transports.Console(),
+    new MongoDB({
+      db: process.env.DB_URL as string,
+      collection: "log_erros",
+      level: "error",
+      options: { useUnifiedTopology: true },
+      metaKey: "meta"
+    }) as unknown as winston.transport
+  ],
   exitOnError: false
 })
 
